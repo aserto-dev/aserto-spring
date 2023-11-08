9985ceb3ce993b12c5df2ea9a389ed986dcdcde1
Bogdan Irimie <bogdan.irimie@aserto.com>
2023-11-08 14:10:51 +0200

    Add path level authorization for all routes.

--- examples/java-application/src/main/java/com/aserto/example/SecurityConfiguration.java
+++ examples/java-application/src/main/java/com/aserto/example/SecurityConfiguration.java
@@ -23,12 +23,22 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable);
+//            This code can be commented out to enable route level authorization
 //            .authorizeHttpRequests(authorize -> authorize
+//                    .requestMatchers(HttpMethod.DELETE, "/todos/{id}")
+//                    .access(new CheckConfig(authzCfg, "group", "admin", "member").getAuthManager())
+//
 //                    .requestMatchers(HttpMethod.GET, "/todos")
 //                    .access(new CheckConfig(authzCfg, "group", "viewer", "member").getAuthManager())
 //
+//                    .requestMatchers(HttpMethod.GET, "/users/{userID}")
+//                    .access(new CheckConfig(authzCfg, "group", "viewer", "member").getAuthManager())
+//
 //                    .requestMatchers(HttpMethod.POST, "/todos")
-//                    .access(new CheckConfig(authzCfg, "group", "admin", "member").getAuthManager())
+//                    .access(new CheckConfig(authzCfg, "group", "editor", "member").getAuthManager())
+//
+//                    .requestMatchers(HttpMethod.PUT, "/todos/{id}")
+//                    .access(new CheckConfig(authzCfg, "group", "editor", "member").getAuthManager())
 //
 //                    .anyRequest().denyAll()
 //        );
